ea2ced6294eac1d1c5f0f54531976d4a5c450691
ExodusGemini <contact@jameshalliday.net>
2016-09-29 11:24:36 -0400

    Fixed math.py

--- math.py
+++ math.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python
-print("Type A for addition, S for subtraction, M for multiplication, or D for division!")
-n_calc = raw_input("/ ")
-if n_calc == "A":
-	n_one = input("Number one: ")
-	n_two = input("Number two: ")
-	print(n_one + n_two);
-	quit();
-elif n_calc == "S":
-	n_one = input("Number one: ")
-	n_two = input("Number two: ")
-	print(n_one - n_two);
-	quit();
-elif n_calc == "M":
-	n_one = input("Number one: ")
-	n_two = input("Number two: ")
-	print(n_one * n_two);
-	quit();
-elif n_calc == "D":
-	n_one = input("Number one: ")
-	n_two = input("Number two: ")
-	print(n_one / n_two);
-	quit();
-else:
-	quit();
+def calculator():
+	print("Type A for addition, S for subtraction, M for multiplication, or D for division!")
+	n_calc = raw_input("/ ")
+	if n_calc == "A":
+		n_one = input("Number one: ")
+		n_two = input("Number two: ")
+		print(n_one + n_two);
+		print("\n")
+		calculator();
+	elif n_calc == "S":
+		n_one = input("Number one: ")
+		n_two = input("Number two: ")
+		print(n_one - n_two);
+		print("\n")
+		calculator();
+	elif n_calc == "M":
+		n_one = input("Number one: ")
+		n_two = input("Number two: ")
+		print(n_one * n_two);
+		print("\n")
+		calculator()
+	elif n_calc == "D":
+		n_one = input("Number one: ")
+		n_two = input("Number two: ")
+		print(n_one / n_two);
+		print("\n")
+		calculator()
+	else:
+		quit();
+
+calculator()
